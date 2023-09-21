541341901f9482634f5f3f39cfe1c1e870f5c458
ankit gupta <ankitgupta17878@gmail.com>
2023-09-21 22:01:00 +0530

    80 % complete product page : Data : 21/9/23

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
 import FormatPrice from "./FormatPrice";
 
 
--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -1,8 +1,8 @@
 import { createContext, useContext, useEffect, useReducer } from "react";
 import axios from "axios";
-import reducer from "../reducer/productReducer";
+import reducer from "../reducer/ProductReducer";
 
-const AppContext = createContext(); 
+const AppContext = createContext();
 
 const API = "https://api.pujakaitem.com/api/products";
 
@@ -19,12 +19,10 @@ const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const getProducts = async (url) => {
-
     dispatch({ type: "SET_LOADING" });
     try {
       const res = await axios.get(url);
       const products = await res.data;
-   
       dispatch({ type: "SET_API_DATA", payload: products });
     } catch (error) {
       dispatch({ type: "API_ERROR" });
